ff9a51d79d5c79fe3176b87b5f5528f4379624e9
hlz2516 <714251494@qq.com>
2023-05-20 14:55:28 +0800

    upd notes

--- AutoSizeHelper/AutoSizeHelper/AutoSizeHelper.cs
+++ AutoSizeHelper/AutoSizeHelper/AutoSizeHelper.cs
@@ -13,17 +13,11 @@ namespace AutoSizeHelper
         public double hRate;
         public double fontRate;
     }
-    /// <summary>
-    /// 分辨率自适应辅助类。
-    /// 当一个窗体或控件发生大小变化时，其内部的多数控件会根据其在原设计器中的位置和大小比例进行自适应变化
-    /// </summary>
+
     public class AutoSizeHelper
     {
         private Control? _container;
-
         private Dictionary<string, ScaleRate> scaleMap;
-
-        //容器原设计大小集合
         private Dictionary<string, Size> ContainerDesignSizes;
 
         public AutoSizeHelper()
@@ -32,9 +26,10 @@ namespace AutoSizeHelper
             ContainerDesignSizes = new Dictionary<string, Size>();
         }
         /// <summary>
-        /// 设置容器。当容器触发SizeChanged事件时，容器内的控件会自动进行大小，位置和字体属性的自适应变化。
+        /// SetContainer.When the container triggers the SizeChanged event, the controls inside the container will 
+        /// automatically undergo adaptive changes in size, position, and font properties.
         /// </summary>
-        /// <param name="container">容器</param>
+        /// <param name="container">Container Control</param>
         public void SetContainer(Control container)
         {
             _container = container;
@@ -70,19 +65,16 @@ namespace AutoSizeHelper
                     queue.Enqueue(ctrl);
                 }
 
-                //如果当前控件是容器，则加入到ContainerDesignSizes
                 if (curCtrl is ContainerControl || curCtrl is Panel)
                 {
                     ContainerDesignSizes.Add(curCtrl.Name, curCtrl.Size);
                 }
 
-                //对于一开始传入的容器不处理
                 if (curCtrl == _container)
                 {
                     continue;
                 }
 
-                //计算当前控件相对父容器的大小和位置比例，存储入map中
                 var scaleRate = new ScaleRate
                 {
                     xRate = curCtrl.Location.X * 1.0 / curCtrl.Parent.Width,
@@ -96,7 +88,7 @@ namespace AutoSizeHelper
         }
 
         /// <summary>
-        /// 更新容器内的控件属性
+        /// Update control properties within the container.It is usually called after adding a new control.
         /// </summary>
         public void UpdateControls()
         {
@@ -123,7 +115,7 @@ namespace AutoSizeHelper
                 {
                     queue.Enqueue(ctrl);
                 }
-                //对于一开始传入的容器不处理
+
                 if (curCtrl == _container)
                 {
                     continue;
@@ -131,7 +123,6 @@ namespace AutoSizeHelper
 
                 if (scaleMap.ContainsKey(curCtrl.Name))
                 {
-                    //根据map中存储的当前控件大小和位置比例，还原大小和位置
                     var scaleRate = scaleMap[curCtrl.Name];
                     int newX = (int)Math.Round(scaleRate.xRate * curCtrl.Parent.Width);
                     int newY = (int)Math.Round(scaleRate.yRate * curCtrl.Parent.Height);
@@ -147,22 +138,20 @@ namespace AutoSizeHelper
                 }
             }
 
-            //刷新界面
             _container.Invalidate();
         }
 
         /// <summary>
-        /// 向容器内添加新的子控件。添加后该控件具有自适应的功能
+        /// Add a new child control to the container. After adding, the control will adapt to the current layout.
         /// </summary>
-        /// <param name="ctrl">需要自适应功能的子控件</param>
+        /// <param name="ctrl"></param>
         public void AddNewControl(Control ctrl)
         {
-            //找到该控件的容器原设计大小
             if (ctrl.Parent != null)
             {
                 string parentName = ctrl.Parent.Name;
                 Size parentDesignSize = ContainerDesignSizes[parentName];
-                //计算位置和大小比例，再加入到map中
+
                 var scaleRate = new ScaleRate
                 {
                     xRate = ctrl.Location.X * 1.0 / parentDesignSize.Width,
--- AutoSizeHelper/Demo/Form1.cs
+++ AutoSizeHelper/Demo/Form1.cs
@@ -21,10 +21,8 @@ namespace Demo
             newBtn.Location = new Point(568, 12);
             newBtn.Size = new System.Drawing.Size(75, 23);
             newBtn.Text = "button7";
-            //calcul button6's font rate and apply this rate to newBtn font
-            float fontRate = button6.Font.Size / this.Height;
-            float newBtnSize = (float)Math.Round(this.Height * fontRate);
-            newBtn.Font = new Font(button6.Font.FontFamily, newBtnSize);
+            //apply button6's font to newBtn font
+            newBtn.Font = new Font(button6.Font.FontFamily, button6.Font.Size);
             newBtn.UseVisualStyleBackColor = true;
             this.Controls.Add(newBtn);
             helper.AddNewControl(newBtn);
